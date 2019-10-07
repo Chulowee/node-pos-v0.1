d62b135fe04b79fd5d05e6638df2f3cf55d9121d
KATRINA CHLOE REYES (SHP-IRIS-ISD-OOCLL/MNL) <REYESKA@OOCL.COM>
2019-10-07 13:29:46 +0800

    JS Baseline Test

--- main/main.js
+++ main/main.js
@@ -1,4 +1,21 @@
-module.exports = function main() {
+module.exports = function main(inputs) {
     console.log("Debug Info");
-    return 'Hello World!';
+    let header = "***<store earning no money>Receipt ***\n";
+    let item = [];
+    let result = "";
+   
+        inputs.forEach(element => {
+            let elementExists = item.find(coll => element.Barcode == coll.Barcode);
+            if(elementExists){
+                elementExists.Quantity++;
+            } else {
+                item.push({ Name: element, Quantity: 1, price: element.price})
+            }
+        });
+
+
+        item.forEach(element => {
+            result += "Name: "+ String(element.Name) +", Quantity:" ;
+        });
+    return header + result;
 };
