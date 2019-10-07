2006245f112708ca2c3342e8e3db238b37a3735a
KATRINA CHLOE REYES (SHP-IRIS-ISD-OOCLL/MNL) <REYESKA@OOCL.COM>
2019-10-07 18:57:38 +0800

    Finished and fixed failed test

--- main/main.js
+++ main/main.js
@@ -3,31 +3,38 @@ module.exports = function main(inputs) {
     console.log("Debug Info");
     const header = "***<store earning no money>Receipt ***\n";
     let product = [];
-    let subtotal = 0;
     let receipt = "";
     
    
-    inputs.forEach(element => {
-            let elementExists = product.find(prod => prod.Name == element.Name);
+    inputs.forEach(productItem => {
+            let elementExists = product.find(prod => prod.Name == productItem.Name);
             if(elementExists){
                 elementExists.Quantity++;
-                subtotal += element.Price.toFixed(2);
             } else {
-                product.push({Name :element.Name, Quantity: 1, Price: element.Price});
-                subtotal = element.Price;
+                product.push({Name :productItem.Name, Quantity: 1, 
+                    Price: productItem.Price, Unit: productItem.Unit});
             }
         });
-
-
-        product.forEach(element => {
-            receipt += "Name: "+ String(element.Name) 
-            +", Quantity: "+ String(element.Quantity)
-            +", Unit price: "+ element.Price.toFixed(2) + " (yuan)"
-            +", Subtotal:" + subtotal.toFixed(2) + " (yuan)" + "\n" ;
+    let total = 0;
+    product.forEach(productItem => {
+            let subtotal = productItem.Quantity * productItem.Price; 
+            total+= subtotal;
+            receipt += "Name: "+ String(productItem.Name) 
+            +", Quantity: "+ String(productItem.Quantity) + getProductUnit(productItem)
+            +", Unit price: "+ productItem.Price.toFixed(2) + " (yuan)"
+            +", Subtotal: " + subtotal.toFixed(2) + " (yuan)"+ "\n" ;
         });
 
     const footer = '----------------------\n' +
-    'Total: '+subtotal+' (yuan)\n' +
+    'Total: '+total.toFixed(2)+' (yuan)\n' +
     '**********************\n';
     return header + receipt +footer;
+};
+
+function getProductUnit(productItem){
+    if(productItem.Unit == "bottle"){
+        return " bottles";
+    }else{
+        return "";
+    }
 };
